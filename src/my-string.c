ebdda3ee6b5e794d977d03d673a75b78c9ba24f7
Emele-Winner2 <winneremele6@gmail.com>
2026-05-13 10:26:19 +0100

    Added strcpy function to my-string.c

--- src/my-string.c
+++ src/my-string.c
@@ -1,30 +1,31 @@
 #include "../include/my-string.h"
 #include "stdint.h"
 #include "stdbool.h"
-size_t my_strlen(const char *str) {
-  size_t count = 0;
+size_t my_strlen(const char *str)
+{
+	size_t count = 0;
 
-  while(str[count] != '\0') {
-    count++;
-  } 
-  return count;
-
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	return count;
 };
- int my_strcpy(const char* src, char* dest) {
+int my_strcpy(const char *src, char *dest)
+{
 	if (my_strlen(dest) < my_strlen(src))
 	{
 		printf("Error: compare the length of the strings before you copy.\n");
-		return -1;//second string cannot contain src string
+		return -1; // second string cannot contain src string
 	}
-	//copy the elements from src to dest string.
+	// copy the elements from src to dest string.
 	for (size_t i = 0; i < my_strlen(dest); i++)
 	{
-		dest[i] = src[i];//added the null terminator.
-		if (i>my_strlen(src))
+		dest[i] = src[i]; // added the null terminator.
+		if (i > my_strlen(src))
 		{
 			dest[i] = '\0';
 		}
-
 	}
 	return true;
 }
